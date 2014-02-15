6bb3b0def5755172ddf7e0c70a370979e0966d44
Jimmy Zhang <tuesmerde@gmail.com>
2014-02-15 10:00:45 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playz::Application.config.secret_key_base = 'ea817ccf733fe5a0ab518a860884af09a73042dbe0f18c75d124c0c36bfa0cb6347e4d81ce164a267ed30ae708fc44ec5137a71869635becc3b3cb79d9a4bb22'
